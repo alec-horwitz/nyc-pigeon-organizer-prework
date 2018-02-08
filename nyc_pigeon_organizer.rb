166f05279debc70a96004751a06dde329a871b33
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-02-08 03:02:19 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,20 @@
 def nyc_pigeon_organizer(data)
-  pigeon_list = []
-  data[:gender].each {|gender, pigeons| pigeons.each {|pigeon| pigeon_list.push(pigeon)} }
+  allPigeonNames = []
+  newData = {}
+  data[:gender].each {|gender, pigeons| pigeons.each {|pigeon| allPigeonNames.push(pigeon)}}
+  allPigeonNames.each {|pigeonName|
+    data.each {|attribute, statuses|
+      statuses.each {|status, pigeon|
+        if pigeonName == pigeon
+          if newData[pigeonName][attribute].push(status.to_s)
+          else
+            newData[pigeonName][attribute] = [status.to_s]
+            
+          end
+          newData[pigeonName][attribute] 
+        end
+      }
+    }
+  }
   # write your code here!
 end
