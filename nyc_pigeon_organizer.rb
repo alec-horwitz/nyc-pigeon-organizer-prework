b7a203e9d5c4cc80b046d018b9319f31f0e4c456
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-02-08 03:06:58 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,7 +7,7 @@ def nyc_pigeon_organizer(data)
       statuses.each {|status, pigeon|
         if pigeonName == pigeon
           if newData[pigeonName] == false
-            newData[pigeonName] => {}
+            newData[pigeonName] = {}
           end
           if newData[pigeonName][attribute]
             newData[pigeonName][attribute].push(status.to_s)
